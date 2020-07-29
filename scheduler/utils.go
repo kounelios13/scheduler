3004ff7d346dbe7f263936fc3aceb6fedfa11d96
Manos Kounelakis <pkounelios@gmail.com>
2020-07-29 10:43:41 +0300

    added task fn

--- scheduler/utils.go
+++ scheduler/utils.go
@@ -3,11 +3,14 @@ package scheduler
 import (
 	"errors"
 	"fmt"
+	"sync"
 )
 
 // Scheduler struct
 type Scheduler struct {
-	Name string
+	Name  string
+	Tasks []interface{}
+	m     sync.Mutex
 }
 
 // NewScheduler Create an empty scheduler
@@ -16,11 +19,19 @@ func NewScheduler(name string) (*Scheduler, error) {
 		return nil, errors.New("scheduler name is empty")
 	}
 	return &Scheduler{
-		Name: name,
+		Name:  name,
+		Tasks: make([]interface{}, 0),
 	}, nil
 }
 
 // SayHi Say hi
-func (s Scheduler) SayHi() {
+func (s *Scheduler) SayHi() {
 	fmt.Printf("Hello .This is scheduler %s talking \n", s.Name)
+}
+
+// AddTask Add a task
+func (s *Scheduler) AddTask(task interface{}) {
+	s.m.Lock()
+	s.Tasks = append(s.Tasks, task)
+	s.m.Unlock()
 }
